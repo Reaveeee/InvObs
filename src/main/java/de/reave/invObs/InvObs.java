c42447c060e2a9394b8a88b8fdcf89a2348d344b
Reaveeee <reavewt@gmail.com>
2024-11-25 18:53:42 +0100

    Initial commit

---
+++ src/main/java/de/reave/invObs/InvObs.java
@@ -0,0 +1,17 @@
+package de.reave.invObs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InvObs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //getCommand("commandname").setExecutor(new CommandClass());
+        //getServer().getPluginManager().registerEvents(new EventsClass(), this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
